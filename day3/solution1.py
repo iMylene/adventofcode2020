c6e07dd954d921e370e1b2b026c956acff0e475c
iMylene13 <iMylene13@gmail.com>
2020-12-03 11:06:15 +0100

    Add constants

--- day3/solution1.py
+++ day3/solution1.py
@@ -2,20 +2,23 @@ import sys
 
 data = sys.stdin.readlines()
 trees = 0
-r = 1
-c = 3
+r_slope = 1
+c_slope = 3
+
 max_r = len(data) - 1
 max_c = len(data[0]) - 2
 
+r = r_slope
+c = c_slope
 while True:
     if data[r][c] == '#':
         trees += 1
 
-    r+= 1
+    r+= r_slope
     if r > max_r:
         break
 
-    c+= 3
+    c+= c_slope
     if c > max_c:
         c = c - max_c - 1
 
